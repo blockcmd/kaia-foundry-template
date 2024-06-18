a43adbac6ace0db0fe3ad5a2105eb1292553bfff
haxxsniper <haxxsniper@gmail.com>
2024-06-18 18:54:56 +0700

    added more

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,25 +4,29 @@ pragma solidity ^0.8.19;
 import { Script } from "forge-std/Script.sol";
 import { DeployHelper } from "./DeployHelper.s.sol";
 import { Counter } from "../src/Counter.sol";
+import { Monee } from "../src/MoneeToken.sol";
 
 contract Deploy is Script {
-    function run() external returns (Counter, DeployHelper) {
+    function run() external returns (Counter, Monee, DeployHelper) {
         /// @dev initialize the DeployHelper contract
         DeployHelper deployHelper = new DeployHelper();
 
         /// @dev get the active network configuration using the DeployHelper contract 
-        (uint16 startingNumber, address dataFeed) = deployHelper.activeNetworkConfig();
+        (uint16 startingNumber,) = deployHelper.activeNetworkConfig();
 
         /// @dev start the broadcast
         vm.startBroadcast();
 
         /// @dev create a new Counter contract with the active network configuration
-        Counter counter = new Counter(startingNumber, dataFeed);
+        Counter counter = new Counter(startingNumber);
+
+        /// @dev create a new Monee contract with the active network configuration
+        Monee monee = new Monee(0x6FaFF29226219756aa40CE648dbc65FB41DE5F72);
 
         /// @dev stop the broadcast
         vm.stopBroadcast();
 
         /// @dev return the Counter and DeployHelper contracts
-        return (counter, deployHelper);
+        return (counter, monee, deployHelper);
     }
 }
--- test/unit-test/MoneeToken.t.sol
+++ test/unit-test/MoneeToken.t.sol
@@ -2,26 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import { Counter } from "src/Counter.sol";
+import { Monee } from "src/MoneeToken.sol";
 import { DeployHelper } from "script/DeployHelper.s.sol";
 import { Deploy } from "script/Deploy.s.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    Monee public monee;
     DeployHelper public deployHelper;
 
     function setUp() public {
         Deploy deployer = new Deploy();
-        (counter,,) = deployer.run();
-    }
-
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        (,monee,) = deployer.run();
     }
 }
