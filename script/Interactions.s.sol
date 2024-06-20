d22507903570901f3956a8f99ec339550443d288
ZxStim <thang.victor.luong@gmail.com>
2024-06-20 19:33:22 +0700

    made edits

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,14 +3,24 @@ pragma solidity ^0.8.19;
 
 import { Script, console } from "forge-std/Script.sol";
 import { LuckyDraw } from "src/LuckyDraw.sol";
+import { IPrepayment } from "@bisonai/orakl-contracts/v0.1/src/interfaces/IPrepayment.sol";
+import { DevOpsTools } from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract DrawLuckyDraw is Script {
-
-  LuckyDraw luckyDraw = LuckyDraw(0xa5DE8993c063fEfeCCE05e41effF2B17fF8F9DF7);
+  
+  IPrepayment prepayment = IPrepayment(0x8d3A1663d10eEb0bC9C9e537e1BBeA69383194e7);
 
   function run() external {
+    address mostRecentlyDeployedLuckyDrawContract = DevOpsTools.get_most_recent_deployment("LuckyDraw", block.chainid);
+    console.log("Address of LuckyDraw: ", mostRecentlyDeployedLuckyDrawContract);
+    address mostRecentlyDeployedMoneeTokenContract = DevOpsTools.get_most_recent_deployment("Monee", block.chainid);
+    console.log("Address of Monee Token: ", mostRecentlyDeployedMoneeTokenContract);
     vm.startBroadcast();
-    luckyDraw.setMoneeToken(0x58F994D52A56A864Baf0123fA57Fe1336fD1C01b);
+    prepayment.addConsumer(777, 0xc793328d20A95Ff8A6bd96D0B8B0acd039e3d64c);
+    console.log("Added consumer");
+    LuckyDraw luckyDraw = LuckyDraw(0xc793328d20A95Ff8A6bd96D0B8B0acd039e3d64c);
+    luckyDraw.setMoneeToken(0x6161cf6650Dd64c2cBAECd3b92F7B761C26344b2);
+    console.log("Set Monee Token");
     luckyDraw.requestRandomWords();
     vm.stopBroadcast();
     console.log("Drawn LuckyDraw");
