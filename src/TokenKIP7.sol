0ac0e8e68183b28180bb116bdc2e4b52aee043d6
ZxStim <thang.victor.luong@gmail.com>
2024-07-21 02:11:50 +0700

    updated code

--- src/TokenKIP7.sol
+++ src/TokenKIP7.sol
@@ -2,20 +2,19 @@
 pragma solidity ^0.8.4;
 
 import "@kaiachain/contracts/KIP/token/KIP7/KIP7.sol";
-import "lib/kaia-contracts/contracts/access/Ownable.sol";
+import "@kaiachain/contracts/access/Ownable.sol";
 
 /// @title TokenKIP7
 /// @author BlockCMD
 /// @notice A simple KIP7 token that can be minted by its owner
 contract TokenKIP7 is KIP7, Ownable {
-    
     /// -----------------------------------------------------------------------
     /// Constructor
     /// -----------------------------------------------------------------------
 
     /// @notice Constructor to declare token name, symbol and initialOwner
     /// @param initialOwner The amount of underlying tokens to wrap
-    constructor(address initialOwner) KIP7("Token KIP7", "TKIP7") Ownable(initialOwner) { }
+    constructor(address initialOwner) KIP7("Token KIP7", "TKIP7") Ownable() { }
 
     function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
         return super.supportsInterface(interfaceId);
